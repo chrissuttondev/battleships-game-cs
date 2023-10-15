56903f8c5377ac42e696c0e5fd24121d07205427
chrissuttondev <>
2023-10-15 12:59:49 +0000

    Add get shot function to prompt and validate user input

--- run.py
+++ run.py
@@ -1,4 +1,27 @@
+from random import randint
+import random
 print("Welcome to Battleships")
+
+
+def get_shot(guesses):
+    """
+    Prompts user to take a shot. Coverts shot to and integer.
+    Validates input and prompts again if user input not valid. 
+    """
+
+    while True:
+        try:
+            shot = input("\nTake your shot: ")
+            shot = int(shot)
+            if shot < 0 or shot > 99:
+                print("\nMiss-fire, choose number 0-99: ")
+            elif shot in guesses:
+                print("\nAlready fired there, guess again: ")
+            else:
+                break
+        except:
+            print("\nOoops miss-fire, try again: ")
+    return shot
 
 def show_board(hit, miss):
     """
@@ -22,3 +45,23 @@ def show_board(hit, miss):
             row = row + ch
             place = place + 1
         print(x, " ", row)
+
+
+def get_shot(guesses):
+
+    while True:
+        try:
+            shot = input("\nTake your shot: ")
+            shot = int(shot)
+            if shot < 0 or shot > 99:
+                print("\nMiss-fire, choose number 0-99: ")
+            elif shot in guesses:
+                print("\nAlready fired there, guess again: ")
+            else:
+                break
+        except:
+            print("\nOoops miss-fire, try again: ")
+    return shot
+
+
+
