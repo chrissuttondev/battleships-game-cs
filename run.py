5b0318965df17755036b58d5741bf589e173307a
chrissuttondev <>
2023-10-15 13:20:03 +0000

    Added run-game() function to call program functions for game play

--- run.py
+++ run.py
@@ -47,21 +47,68 @@ def show_board(hit, miss):
         print(x, " ", row)
 
 
-def get_shot(guesses):
+def check_shot(shot, ships, hit, miss):
+    """
+    Checks if the users shot is a hit or a miss and appends the shot to
+    a list held in a variables named hit and miss respectively.
+    Prints a message to the terminal to inform user if shot a hit or a miss.
+    """
 
-    while True:
-        try:
-            shot = input("\nTake your shot: ")
-            shot = int(shot)
-            if shot < 0 or shot > 99:
-                print("\nMiss-fire, choose number 0-99: ")
-            elif shot in guesses:
-                print("\nAlready fired there, guess again: ")
-            else:
-                break
-        except:
-            print("\nOoops miss-fire, try again: ")
-    return shot
+    hit_msgs = ["You smashed 'em.",
+                "Great Shot.",
+                "Another one bites the dust.",
+                "They're toast.",
+                "They are fish food.",
+                "Down it goes.",
+                "The sharks feast tonight.",
+                "Surfs up."]
+    random.shuffle(hit_msgs)
+
+    miss_msgs = ["Unlucky.",
+                 "Keep trying",
+                 "Better luck next time",
+                 "What are you even aiming at?",
+                 "Sheesh, miles off!",
+                 "close one",
+                 "You know your supposed hit the ships?",
+                 "Stop wasting ammo!"]
+
+    random.shuffle(miss_msgs)
+
+    if shot in ships:
+        ships.remove(shot)
+        if len(ships) > -1:
+            hit.append(shot)
+            if hit_msgs:
+                random_msg = hit_msgs.pop()
+                print(f"\nDIRECT HIT!\n{random_msg}")
+    else:
+        miss.append(shot)
+        if miss_msgs:
+            random_msg = miss_msgs.pop()
+            print(f"\nYOU MISSED!\n{random_msg}")
+    return ships, hit, miss,
 
 
+def run_game():
+    """
+    Calls the program functions to run the game.
+    """
+
+    ships = random.sample(range(0, 99), 5)
+    print(ships)
+    hit = []
+    miss = []
+    target_length = 20
+
+    for i in range(20):
+        show_board(hit, miss)
+        guesses = hit + miss
+        shot = get_shot(guesses)
+        ships, hit, miss = check_shot(shot, ships, hit, miss)
+        turns = hit + miss
+        if not bool(ships):  # if ships list empty game end you win
+            print("\nYOU WIN! ENEMY VESSELS DESTROYED")
+            break
+        player_lose(turns, target_length)
 
