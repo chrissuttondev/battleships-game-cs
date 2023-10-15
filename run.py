87e73cab3765e8c5f7029487ff3ea129fb32f708
chrissuttondev <>
2023-10-15 14:09:06 +0000

    Edited code in CI Python Lineter. Removed minor formatting erros

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 from random import randint
 import random
 print("Welcome to Battleships.")
-print("There are 5 enemy targets.\nTake your shots by choosing a number from 0-99")
+print("There are 5 enemy targets.")
+print("Take your shots by")
+print("choosing a number from 0-99")
 
 
 def get_shot(guesses):
     """
     Prompts user to take a shot. Coverts shot to and integer.
-    Validates input and prompts again if user input not valid. 
+    Validates input and prompts again if user input not valid.
     """
 
     while True:
@@ -20,15 +22,16 @@ def get_shot(guesses):
                 print("\nAlready fired there, guess again: ")
             else:
                 break
-        except:
+        except ValueError:
             print("\nOoops miss-fire, try again: ")
     return shot
+
 
 def show_board(hit, miss):
     """
     Displays game board in terminal. The board has 99 places.
-    When a user takes a it will be indicated on the board wether 
-    the shot was a hit (H) or a miss (M). 
+    When a user takes a it will be indicated on the board wether
+    the shot was a hit (H) or a miss (M).
     """
     print("\n        BATTLESHIPS")
     print("\n     0 1 2 3 4 5 6 7 8 9")
@@ -116,12 +119,13 @@ def run_game():
 
 def player_lose(turns, target_length):
     """
-    Checks the length of the list stored in the turns variable. 
-    If the list has reached the target lenght thee game and ends 
+    Checks the length of the list stored in the turns variable.
+    If the list has reached the target lenght thee game and ends
     and the player loses as not all ships have been hit.
     """
     if len(turns) == target_length:
         print("\nYOU LOSE! ENEMY THREAT REMAINS")
+
 
 run_game()
 print("\nGAME END")
